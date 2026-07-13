129f89f9662ff67aa23e10c236af3aa80c9614a1
eldora25 <32362177+eldora25@users.noreply.github.com>
2026-07-13 20:51:03 +0300

    Update GoalScreen.kt

--- app/src/main/java/com/example/kelimehatirlatici/ui/GoalScreen.kt
+++ app/src/main/java/com/example/kelimehatirlatici/ui/GoalScreen.kt
@@ -1,5 +1,6 @@
 package com.example.kelimehatirlatici.ui
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
